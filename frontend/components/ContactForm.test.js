9d4a38d99bdf871dbb9a016192751b27c6131449
Ben Johnson <monk.sound.design@gmail.com>
2023-06-21 22:46:11 -0400

    removed display component import

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -3,7 +3,6 @@ import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
-import DisplayComponent from './DisplayComponent';
 
 test('renders without errors', () => {
     render(<ContactForm />);
